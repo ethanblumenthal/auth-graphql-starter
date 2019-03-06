5b0899d4e9f0f00a60fe27065b48e3e4ed007e37
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-03-06 13:20:21 -0600

    created UserType and mutations

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://ethan:password1@ds161285.mlab.com:61285/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
